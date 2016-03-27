9ce7e3a57eb4da732d6e03d751f86625da37eb2b
Jonathan Sick <jsick@lsst.org>
2016-03-26 20:10:15 -0700

    PEP8 fixes via lsst-autopep8
    PEP8 errors ignored:
    
    - E133
    - E226
    - E228
    - E251
    - N802
    - N803
    - W391
    
    Max line length setting: 110

--- bin/CreateProject.py
+++ bin/CreateProject.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
-# 
+#
 # LSST Data Management System
 # Copyright 2008, 2009 LSST Corporation.
-# 
+#
 # This product includes software developed by the
 # LSST Project (http://www.lsst.org/).
 #
@@ -10,14 +10,14 @@
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
-# You should have received a copy of the LSST License Statement and 
-# the GNU General Public License along with this program.  If not, 
+#
+# You should have received a copy of the LSST License Statement and
+# the GNU General Public License along with this program.  If not,
 # see <http://www.lsstcorp.org/LegalNotices/>.
 #
 import os
@@ -32,13 +32,13 @@ import datetime
 #
 # INPUT:
 #	-p   <eupsPackageName>  e.g. daf_base | pex_exceptions | base
-#       -e   <developerEmail>   e.g. robyn@LSST.org 
+#       -e   <developerEmail>   e.g. robyn@LSST.org
 #       -b   Select BugDetective ruleset
 #       -r   select LSST Required Rules
 #       -d   select LSST Desirable Rules
 #
 # OUTPUT:
-# 
+#
 ####################################################################################
 
 # Following is the presumptive directory structure for C++Test processing
@@ -68,8 +68,8 @@ import datetime
 usage = """%prog [ -e email ]  eupsModule"""
 
 parser = optparse.OptionParser()
-parser.add_option("-e", "--email", action="store", 
-		  dest="email", metavar="EMAIL",
+parser.add_option("-e", "--email", action="store",
+                  dest="email", metavar="EMAIL",
                   default="robyn@LSST.org", help="send report to EMAIL ")
 
 parser.opts = {}
@@ -79,8 +79,8 @@ parser.args = []
 (parser.opts, parser.args) = parser.parse_args()
 if len(parser.args) < 1:
     print usage
-    sys.stderr.write ('Syntax error\n')
-    sys.exit (1)
+    sys.stderr.write('Syntax error\n')
+    sys.exit(1)
 
 package = parser.args[0]
 email = parser.opts.email
@@ -88,8 +88,8 @@ email = parser.opts.email
 developer = "cppTestbed"
 home = "/home/buildbot"
 
-developerHome = "%s/%s" % (home,  developer)
-workspace =  "%s/workspaceCpptest" % (developerHome)
+developerHome = "%s/%s" % (home, developer)
+workspace = "%s/workspaceCpptest" % (developerHome)
 svnPackageDir = "%s/sourceSvn/%s" % (developerHome, package)
 ceReports = "%s/reportCpptest" % (developerHome)
 subjectEmail = "Cpptest ...%s...  Standards Check log" % (package)
@@ -100,12 +100,12 @@ ceLocalSettings = "%s/localSettingsCpptest.def" % (developerHome)
 ceExcludes = "%s/globalExcludesCpptest.lst" % (home)
 nowStruct = datetime.datetime.now()
 now = nowStruct.strftime("%Y%m%dT%H%M%S")
-cePackageReport = "%s/%s/%s" % (ceReports,package,now)
+cePackageReport = "%s/%s/%s" % (ceReports, package, now)
 cePackageReportLog = "%s/Report.log" % (cePackageReport)
 
 print "Developer == %s" % (developer)
 print "Developer's home == %s" % (developerHome)
-print "SVN Package  == C++Test Project == %s" % (package )
+print "SVN Package  == C++Test Project == %s" % (package)
 print ""
 
 print "SVN Package path ==  %s" % (svnPackageDir)
@@ -121,9 +121,9 @@ print "CE Package Report Log == %s" % (cePackageReportLog)
 print ""
 
 
-# Build the DateTime-stamped Report subdirectory 
+# Build the DateTime-stamped Report subdirectory
 cmd = "mkdir -p %s" % (cePackageReport)
-print "COMMAND: %s" %(cmd)
+print "COMMAND: %s" % (cmd)
 
 try:
     retcode = call(cmd, shell=True)
@@ -138,8 +138,9 @@ except OSError, e:
 
 
 # Generate the appropriate Project Creation report
-cmd = 'cpptestcli -bdf %s -data %s -resource %s -config "%s" -exclude %s -localsettings %s -report %s -showdetails -appconsole stdout -publish -nobuild &> %s' % (ceBdf, workspace, package, rules, ceExcludes, ceLocalSettings, cePackageReport, cePackageReportLog)
-print "COMMAND: %s" %(cmd)
+cmd = 'cpptestcli -bdf %s -data %s -resource %s -config "%s" -exclude %s -localsettings %s -report %s -showdetails -appconsole stdout -publish -nobuild &> %s' % (
+    ceBdf, workspace, package, rules, ceExcludes, ceLocalSettings, cePackageReport, cePackageReportLog)
+print "COMMAND: %s" % (cmd)
 
 try:
     retcode = call(cmd, shell=True)
@@ -154,8 +155,8 @@ except OSError, e:
 
 
 # mail the Project Creation Log to the user
-cmd =  'mail -s "%s" %s < %s' % (subjectEmail, email, cePackageReportLog)
-print "COMMAND: %s" %(cmd)
+cmd = 'mail -s "%s" %s < %s' % (subjectEmail, email, cePackageReportLog)
+print "COMMAND: %s" % (cmd)
 
 try:
     retcode = call(cmd, shell=True)
--- bin/CreateReport.py
+++ bin/CreateReport.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
-# 
+#
 # LSST Data Management System
 # Copyright 2008, 2009 LSST Corporation.
-# 
+#
 # This product includes software developed by the
 # LSST Project (http://www.lsst.org/).
 #
@@ -10,14 +10,14 @@
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
-# You should have received a copy of the LSST License Statement and 
-# the GNU General Public License along with this program.  If not, 
+#
+# You should have received a copy of the LSST License Statement and
+# the GNU General Public License along with this program.  If not,
 # see <http://www.lsstcorp.org/LegalNotices/>.
 #
 import os
@@ -31,14 +31,14 @@ import datetime
 # Generate a new C++Test Report
 #
 # INPUT:
-#       -e   <developerEmail>   e.g. robyn@LSST.org 
+#       -e   <developerEmail>   e.g. robyn@LSST.org
 #       -b   Select C++ BugDetective ruleset
 #       -r   Select C++ LSST Required Rules
 #       -d   Select C++ LSST Desirable Rules
 #       -p   Select Python pylint Rules
 #
 # OUTPUT:
-# 
+#
 ####################################################################################
 
 # Following is the presumptive directory structure for C++Test processing
@@ -68,8 +68,8 @@ import datetime
 usage = """%prog [ -brdp ] [ -e email ]  eupsModule"""
 
 parser = optparse.OptionParser()
-parser.add_option("-e", "--email", action="store", 
-		  dest="email", metavar="EMAIL",
+parser.add_option("-e", "--email", action="store",
+                  dest="email", metavar="EMAIL",
                   default="robyn@LSST.org", help="send report to EMAIL ")
 
 parser.add_option("-r", "--required", action="store_true",
@@ -84,7 +84,7 @@ parser.add_option("-b", "--bug", action="store_true",
                   dest="bug", metavar="BUG",
                   default=False, help="use Bug Detective Rules")
 
-parser.add_option("-p","--python", action="store_true",
+parser.add_option("-p", "--python", action="store_true",
                   dest="python", metavar="PYTHON",
                   default=False, help="use pylint")
 
@@ -95,14 +95,14 @@ parser.args = []
 (parser.opts, parser.args) = parser.parse_args()
 if len(parser.args) < 1:
     print usage
-    sys.stderr.write ('Syntax error\n')
-    sys.exit (1)
+    sys.stderr.write('Syntax error\n')
+    sys.exit(1)
 
 package = parser.args[0]
 email = parser.opts.email
 rules = "user://LSST Required"
 
-if parser.opts.bug :
+if parser.opts.bug:
     rules = "builtin://BugDetective (License Required)"
 elif parser.opts.desirable:
     rules = "user://LSST Recommended"
@@ -110,8 +110,8 @@ elif parser.opts.desirable:
 developer = "cppTestbed"
 home = "/home/buildbot"
 
-developerHome = "%s/%s" % (home,  developer)
-workspace =  "%s/workspaceCpptest" % (developerHome)
+developerHome = "%s/%s" % (home, developer)
+workspace = "%s/workspaceCpptest" % (developerHome)
 svnPackageDir = "%s/sourceSvn/%s" % (developerHome, package)
 ceReports = "%s/reportCpptest" % (developerHome)
 
@@ -120,14 +120,14 @@ ceLocalSettings = "%s/localSettingsCpptest.def" % (developerHome)
 ceExcludes = "%s/globalExcludesCpptest.lst" % (home)
 nowStruct = datetime.datetime.now()
 now = nowStruct.strftime("%Y%m%dT%H%M%S")
-cePackageReport = "%s/%s/%s" % (ceReports,package, now)
+cePackageReport = "%s/%s/%s" % (ceReports, package, now)
 cePackageReportCppLog = "%s/Report.log" % (cePackageReport)
 cePackageReportBdfLog = "%s/Bdf.log" % (cePackageReport)
-cePackageReportPythonLog = "%s/%sPython.txt" % (cePackageReport,package)
+cePackageReportPythonLog = "%s/%sPython.txt" % (cePackageReport, package)
 
 print "Developer == %s" % (developer)
 print "Developer's home == %s" % (developerHome)
-print "SVN Package  == C++Test Project == %s" % (package )
+print "SVN Package  == C++Test Project == %s" % (package)
 print ""
 
 print "SVN Package path ==  %s" % (svnPackageDir)
@@ -143,9 +143,9 @@ print "CE Package Report C++ Log == %s" % (cePackageReportCppLog)
 print "CE Package Report Python Log == %s" % (cePackageReportPythonLog)
 print ""
 
-# Build the DateTime-stamped Report subdirectory 
+# Build the DateTime-stamped Report subdirectory
 cmd = "mkdir %s" % (cePackageReport)
-print "COMMAND: %s" %(cmd)
+print "COMMAND: %s" % (cmd)
 
 
 try:
@@ -160,12 +160,13 @@ except OSError, e:
     sys.exit(0)
 
 
-if parser.opts.python :
+if parser.opts.python:
     # Generate the appropriate Python Standards Checking report
-    cmd = 'cd %s; find . -name .tests -prune -o -name "*[^Lib].py" -exec pylint \{\} \\; &> %s' % (svnPackageDir, cePackageReportPythonLog )
-    
-    print "COMMAND: %s" %(cmd)
-    
+    cmd = 'cd %s; find . -name .tests -prune -o -name "*[^Lib].py" -exec pylint \{\} \\; &> %s' % (
+        svnPackageDir, cePackageReportPythonLog)
+
+    print "COMMAND: %s" % (cmd)
+
     try:
         retcode = call(cmd, shell=True)
         if retcode < 0:
@@ -176,11 +177,11 @@ if parser.opts.python :
     except OSError, e:
         print >>sys.stderr, "Python Report Generation failed:", e
         sys.exit(0)
-    
+
     # mail the Python Report to the user
-    cmd =  'mail -s "%s Python Standards Check" %s < %s' % (package, email, cePackageReportPythonLog)
-    print "COMMAND: %s" %(cmd)
-    
+    cmd = 'mail -s "%s Python Standards Check" %s < %s' % (package, email, cePackageReportPythonLog)
+    print "COMMAND: %s" % (cmd)
+
     try:
         retcode = call(cmd, shell=True)
         if retcode < 0:
@@ -191,14 +192,15 @@ if parser.opts.python :
     except OSError, e:
         print >>sys.stderr, "Mailing of Python Report failed:", e
         sys.exit(0)
-    
-else :  
+
+else:
     # Process C++ Standards Check
 
     # First create a clean cpptest *.bdf file
-    cmd = 'cd %s; rm -f %s; scons -c; scons setenv=1 CXX="cpptestscan g++" &> %s' % (svnPackageDir,ceBdf,cePackageReportBdfLog)
+    cmd = 'cd %s; rm -f %s; scons -c; scons setenv=1 CXX="cpptestscan g++" &> %s' % (
+        svnPackageDir, ceBdf, cePackageReportBdfLog)
 
-    print "COMMAND: %s" %(cmd)
+    print "COMMAND: %s" % (cmd)
 
     try:
         retcode = call(cmd, shell=True)
@@ -212,10 +214,11 @@ else :
         sys.exit(0)
 
     # Generate the appropriate C++ Standards Checking report
-    cmd = 'cpptestcli -bdf %s -data %s -resource %s -config "%s" -exclude %s -localsettings %s -report %s -showdetails -appconsole stdout -publish -nobuild &> %s' % (ceBdf, workspace, package, rules, ceExcludes, ceLocalSettings, cePackageReport, cePackageReportCppLog)
-    
-    print "COMMAND: %s" %(cmd)
-    
+    cmd = 'cpptestcli -bdf %s -data %s -resource %s -config "%s" -exclude %s -localsettings %s -report %s -showdetails -appconsole stdout -publish -nobuild &> %s' % (
+        ceBdf, workspace, package, rules, ceExcludes, ceLocalSettings, cePackageReport, cePackageReportCppLog)
+
+    print "COMMAND: %s" % (cmd)
+
     try:
         retcode = call(cmd, shell=True)
         if retcode < 0:
@@ -226,11 +229,11 @@ else :
     except OSError, e:
         print >>sys.stderr, "C++ Report Generation failed:", e
         sys.exit(0)
-    
+
     # mail the C++ Report Generation Log to the user
-    cmd =  'mail -s "%s C++ Standards Check Log" %s < %s' % (package, email, cePackageReportCppLog)
-    print "COMMAND: %s" %(cmd)
-    
+    cmd = 'mail -s "%s C++ Standards Check Log" %s < %s' % (package, email, cePackageReportCppLog)
+    print "COMMAND: %s" % (cmd)
+
     try:
         retcode = call(cmd, shell=True)
         if retcode < 0:
@@ -241,4 +244,4 @@ else :
     except OSError, e:
         print >>sys.stderr, "Mailing of C++ Report failed:", e
         sys.exit(0)
-    
+
